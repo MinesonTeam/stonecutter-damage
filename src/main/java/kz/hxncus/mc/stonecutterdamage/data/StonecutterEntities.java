dd055e90e8f644d7249ed85018ee08c6846639a3
Hxncus <62509061+Hxncusik@users.noreply.github.com>
2026-03-22 19:28:08 +0500

    Revert "chore(release): prepare v1.1.0 (#19)" (#22)
    This reverts commit eac1d7e63fbaa760f384e03398b234f00a4f71fa.

--- src/main/java/kz/hxncus/mc/stonecutterdamage/data/StonecutterEntities.java
+++ src/main/java/kz/hxncus/mc/stonecutterdamage/data/StonecutterEntities.java
@@ -5,7 +5,6 @@ import org.bukkit.util.BlockVector;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 /**
  * StonecutterEntities part of the StonecutterDamage Minecraft plugin.
@@ -14,17 +13,17 @@ import java.util.UUID;
  * @since 1.0.0
  */
 public class StonecutterEntities {
-    private final Map<UUID, BlockVector> entityBlockCache = new HashMap<>();
+    private final Map<LivingEntity, BlockVector> entityBlockCache = new HashMap<>();
 
     public BlockVector get(LivingEntity entity) {
-        return entityBlockCache.get(entity.getUniqueId());
+        return entityBlockCache.get(entity);
     }
 
     public BlockVector put(LivingEntity entity, BlockVector vector) {
-        return entityBlockCache.put(entity.getUniqueId(), vector);
+        return entityBlockCache.put(entity, vector);
     }
 
     public BlockVector remove(LivingEntity entity) {
-        return entityBlockCache.remove(entity.getUniqueId());
+        return entityBlockCache.remove(entity);
     }
 }
